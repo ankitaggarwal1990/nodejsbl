fd0ff8911c1bc24042d2e40e7976e26eb2fcdcbf
ankitaggarwal1990 <aggarwal.ankit007@gmail.com>
2017-02-10 17:35:25 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,8 +1,11 @@
 var config = {}
 
-config.host = process.env.HOST || "https://tridenttrackandtraceba5d1.documents.azure.com:443/";
-config.authKey = process.env.AUTH_KEY || "ut72OQFzNM72qVHRcD68AVxQR7KK8BOqmfSTolqDiMsCYEKFSwfvV5Cfpf5LGy2IxVf9iCejORJx7QBFdt906w==";
+config.host = process.env.HOST || "[https://tridenttrackandtraceba5d1.documents.azure.com:443/]";
+config.authKey = process.env.AUTH_KEY || "[ut72OQFzNM72qVHRcD68AVxQR7KK8BOqmfSTolqDiMsCYEKFSwfvV5Cfpf5LGy2IxVf9iCejORJx7QBFdt906w==]";
 config.databaseId = "DevMgmtDB";
 config.collectionId = "rfid";
+
+
+hghgjhgjhgjgjg
 
 module.exports = config;
